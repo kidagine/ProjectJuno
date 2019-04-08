4fb7a96a07c187fe2690128ddeb222705e874011
kidagine <davidkalatzis1999@hotmail.com>
2019-04-08 07:15:28 +0200

    Added reload functionality with UI and animation, and HP mock bar

--- Assets/Scripts/PlayerScripts/PlayerAim.cs
+++ Assets/Scripts/PlayerScripts/PlayerAim.cs
@@ -1,23 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerAim : MonoBehaviour {
 
 	public Transform firePoint;
 	public GameObject bulletPrefab;
 	public CameraShaker cameraShaker;
+	public Slider gunClipSlider;
+	public Animator gunClipSliderAnim;
 	public static bool isUsingController = false;
 
 	private PlayerMovement playerMovement;
 	private float cooldown = 0.3f;
+	private int gunClip = 10;
+	private bool isReloading;
 
-	void Start ()
+	void Start()
 	{
 		playerMovement = gameObject.GetComponent<PlayerMovement>();
+		gunClipSlider.maxValue = gunClip;
+		gunClipSlider.value = gunClip;
 	}
 
-	void Update ()
+	void Update()
 	{
 		PointingGun();
 		Shooting();
@@ -28,14 +36,41 @@ public class PlayerAim : MonoBehaviour {
 	{
 		if (Input.GetButton("Fire1") && !PauseManager.GameIsPaused)
 		{
-			if (cooldown <= 0)
+			if (cooldown <= 0 && gunClip >= 1 && !isReloading)
 			{
+				gunClip--;
+				gunClipSlider.value -= 1;
 				Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
 				FindObjectOfType<AudioManager>().Play("Shot");
-				cameraShaker.CameraShake();	
+				cameraShaker.CameraShake();
 				cooldown = 0.3f;
 			}
+			else if (gunClip <= 0 && !isReloading)
+			{
+				isReloading = true;
+				FindObjectOfType<AudioManager>().Play("Reload");
+				gunClipSliderAnim.enabled = true;
+				gunClipSliderAnim.Play("Reload", -1, 0f);
+				StartCoroutine(Reload());
+			}
 		}
+
+		if (Input.GetKey(KeyCode.R) && gunClip != 10 && !isReloading)
+		{
+			isReloading = true;
+			FindObjectOfType<AudioManager>().Play("Reload");
+			gunClipSliderAnim.enabled = true;
+			gunClipSliderAnim.Play("Reload" ,- 1, 0f);
+			StartCoroutine(Reload());
+		}
+	}
+	IEnumerator Reload()
+	{
+		yield return new WaitForSeconds(1);
+		gunClip = 10;
+		gunClipSlider.value = gunClip;
+		gunClipSliderAnim.enabled = false;
+		isReloading = false;
 	}
 
 	private void PointingGun()
