283576b4a8150abcfb027558784e7692843de49c
kidagine <davidkalatzis1999@hotmail.com>
2019-05-07 23:27:03 +0200

    +Added initial dash trail. +Added a color lerp for the intro boss eye.

--- Assets/Scripts/EnemyScripts/EnemyAutoAim.cs
+++ Assets/Scripts/EnemyScripts/EnemyAutoAim.cs
@@ -5,10 +5,35 @@ using UnityEngine;
 public class EnemyAutoAim : Enemy {
 
 	public GameObject player;
+	public SpriteRenderer bossEye;
 	Vector2 direction;
+
+	private Color activeAimColor;
+	private Color disabledAimColor;
+	private bool isEyeActive;
+	private float lerpValue = 0;
+
+	void Start()
+	{
+		ColorUtility.TryParseHtmlString("#ff006f", out activeAimColor);
+		ColorUtility.TryParseHtmlString("#ffffff", out disabledAimColor);
+		bossEye.color = disabledAimColor;
+	}
 
 	void Update()
 	{
+		if (!isEyeActive)
+		{
+			bossEye.color = Color.Lerp(disabledAimColor, activeAimColor, Mathf.PingPong(Time.time, lerpValue));
+			if (lerpValue < 1)
+				lerpValue += Time.deltaTime / 1.0f;
+			else
+			{
+				isEyeActive = true;
+				lerpValue = 0.0f;
+			}
+		}
+
 		if (player != null)
 		{
 			direction = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y);
@@ -26,4 +51,5 @@ public class EnemyAutoAim : Enemy {
 	{
 		base.Shoot();
 	}
+
 }
