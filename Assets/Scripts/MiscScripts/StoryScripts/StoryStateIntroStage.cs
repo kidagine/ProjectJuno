4d01edb1b777a94169da8f3cd0fade0709b6f1c3
kidagine <davidkalatzis1999@hotmail.com>
2019-06-14 17:26:17 +0200

    +Changed the way the intro stage cutscene is done by using a timeline which is more appropriate(Also a good reason to learn how to use timelines for the first time).

--- Assets/Scripts/MiscScripts/StoryScripts/StoryStateIntroStage.cs
+++ Assets/Scripts/MiscScripts/StoryScripts/StoryStateIntroStage.cs
@@ -4,62 +4,30 @@ using UnityEngine;
 
 public class StoryStateIntroStage : MonoBehaviour
 {
-
-	[SerializeField] private Animator fadePanelAnimator;
 	[SerializeField] private Animator playerAnimator;
 	[SerializeField] private Animator cameraAnimator;
 	[SerializeField] private PlayerMovement playerMovement;
-	[SerializeField] private GameObject npc;
-
 
 	void Start()
     {
-		FindObjectOfType<AudioManager>().Play("LightFadeOut");
-		FindObjectOfType<AudioManager>().Play("IntroStagePiano");
-		cameraAnimator.SetTrigger("ZoomOut");
 		playerAnimator.SetBool("IsLaying", true);
-		playerAnimator.SetBool("CutsIsSleeping", true);
-		fadePanelAnimator.SetTrigger("FadeIntroStage");
+		playerAnimator.SetTrigger("OpenEyes");
+		playerAnimator.SetTrigger("WakeUp");
+		cameraAnimator.SetTrigger("ZoomOut");
 		playerMovement.enabled = false;
 	}
 
 	void Update()
 	{
-		if (FindObjectOfType<AnimationEventHandler>().GetChangeAnim())
+		if (FindObjectOfType<TimelineSignalInvoker>().TimelineIsOver())
 		{
-			playerAnimator.SetBool("CutsIsSleeping", false);
-			playerAnimator.SetTrigger("OpenEyes");
-		}
-
-		if (FindObjectOfType<AnimationEventHandler>().GetRelease())
-		{
-			playerAnimator.SetTrigger("WakeUp");
 			if (Input.anyKey)
 			{
+				playerAnimator.SetBool("IsLaying", false);
 				playerMovement.enabled = true;
-				playerAnimator.SetBool(("IsLaying"), false);
 				Destroy(this);
 			}
 		}
-
-		if (FindObjectOfType<AnimationEventHandler>().GetActivate())
-		{
-			npc.SetActive(true);
-		}
-		else if (!(FindObjectOfType<AnimationEventHandler>().GetActivate()))
-		{
-			npc.SetActive(false);
-		}
-	}
-
-	public void ShowNPC()
-	{
-		npc.SetActive(true);
-	}
-
-	public void HideNPC()
-	{
-		npc.SetActive(false);
 	}
 
 }
---
+++ Assets/Scripts/MiscScripts/StoryScripts/TimelineSignalInvoker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineSignalInvoker : MonoBehaviour
+{
+	private bool boolValue;
+	private bool isTimelineOver;
+
+	public void SetBoolValue(bool boolValue)
+	{
+		this.boolValue = boolValue;
+	}
+
+	public void PlayerMovementActivation(PlayerMovement playerMovement)
+	{
+		playerMovement.enabled = boolValue;
+	}
+
+	public void SetTimelineIsOver(bool boolValue)
+	{
+		isTimelineOver = boolValue;
+	}
+
+	public bool TimelineIsOver()
+	{
+		return isTimelineOver;
+	}
+
+}
